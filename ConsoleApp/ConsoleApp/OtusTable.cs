96b1cd57d252cd44db5ce8fc74cf2f3cb2eb0e05
khatuntsevke <kirill.khatuntsev@yandex.ru>
2022-08-20 02:55:55 +0500

    add OtusTable configure gitignore update Program.cs

---
+++ ConsoleApp/ConsoleApp/OtusTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class OtusTable
+{
+	private uint dimension;
+	private string text;
+	
+	public OtusTable()
+	{
+        dimension = 0;
+	}
+
+	public OtusTable(uint tableDimension)
+    {
+        dimension = tableDimension;
+    }
+
+	public void Print()
+	{
+
+	}
+
+	private void PrintChessBoard()
+	{
+
+	}
+    
+	private void PrintHourGlass()
+    {
+
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            string? text;
+            do
+            {                
+                Console.Write("Введите произвольный текст: ");
+                text = Console.ReadLine();                
+            }
+            while (text == null || text == "");
+                        
+            string? dimStr;
+            do
+            {
+                Console.Write("Введите целое число от 1 до 6 - размерность таблицы: ");
+                dimStr = Console.ReadLine();
+            }
+            while (!int.TryParse(dimStr, out int dim) || dim<1 || dim>6 );            
+            
+            int tableDimension = int.Parse(dimStr);
+                        
+        }
+    }
+}
